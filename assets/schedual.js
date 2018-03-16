4157033a365e3f723934f89e2cf311cdbf1c43ca
jfleck91 <jfleck91@gmail.com>
2018-03-16 14:44:36 -0700

    first psudocode

---
+++ assets/schedual.js
@@ -0,0 +1,38 @@
+//initialize firebase
+
+
+
+
+//button for adding trains
+
+
+
+
+
+//get user input
+
+
+
+
+//object for holding data
+
+
+
+
+
+//push to data base
+
+
+
+
+
+//log to console
+
+
+
+
+
+
+
+
+
