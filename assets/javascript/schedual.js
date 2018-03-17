19bd83db5d97b7c7ee78f51b807d733945deb4a9
jfleck91 <jfleck91@gmail.com>
2018-03-16 18:20:24 -0700

    update

--- assets/javascript/schedual.js
+++ assets/javascript/schedual.js
@@ -14,27 +14,27 @@ $("#add-train-btn").on("click", function(event) {
     event.preventDefault()
 
 //get user input
-var empName = $("#train-name-input").val().trim();
-var empDestination = $("#destination-input").val().trim();
-var empTime = moment($("#time-input").val().trim(), "HH:mm").format("X");
-var empFrequency = $("#frequency-input").val().trim();
+var trainName = $("#train-name-input").val().trim();
+var trainDestination = $("#destination-input").val().trim();
+var trainTime = moment($("#time-input").val().trim(), "HH:mm").format("X");
+var trainFrequency = $("#frequency-input").val().trim();
 
 //object for holding data
-var newEmp = {
-    name: empName,
-    destination: empDestination,
-    time: empTime,
-    frequency: empFrequency
+var newTrain = {
+    name: trainName,
+    destination: trainDestination,
+    time: trainTime,
+    frequency: trainFrequency
   };
 
 //push to database
-database.ref().push(newEmp);
+database.ref().push(newTrain);
 
 //log to console
-console.log(newEmp.name);
-console.log(newEmp.destination);
-console.log(newEmp.time);
-console.log(newEmp.frequency);
+console.log(trainEmp.name);
+console.log(trainEmp.destination);
+console.log(trainEmp.time);
+console.log(trainEmp.frequency);
 
   // Alert
   alert("Train successfully added");
@@ -45,3 +45,36 @@ console.log(newEmp.frequency);
   $("#time-input").val("");
   $("#frequency-input").val("");
 });
+
+
+//Create Firebase event 
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+// Store everything into a variable.
+var trainName = childSnapshot.val().name;
+var trainDestination = childSnapshot.val().destination;
+var trainTime = childSnapshot.val().time;
+var trainFrequency = childSnapshot.val().frequency;
+
+// Train Info
+console.log(trainempName);
+console.log(trainDestination);
+console.log(trainTime);
+console.log(trainFrequency);
+
+// Prettify
+var trainStartPretty = moment.unix(trainStart).format("HH:mm");
+
+
+//calculate the time
+
+
+
+//Calculate  frequency
+
+
+
+
+// Add each train's data into the table
